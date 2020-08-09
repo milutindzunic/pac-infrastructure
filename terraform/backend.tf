3d6fb63d3f63d062a5473c5078566e00324bd68b
Milutin Dzunic <milutin.dzunic@prodyna.com>
2020-08-09 12:05:57 +0200

    Added initial backend mysql configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,3 +3,37 @@ resource "kubernetes_namespace" "backend" {
     name = "backend"
   }
 }
+
+resource "random_password" "backend-mysql-password" {
+  length = 16
+  special = false
+}
+
+resource "kubernetes_secret" "backend-mysql-access" {
+  metadata {
+    name = "keycloak-backend-access"
+    namespace = kubernetes_namespace.backend.metadata[0].name
+  }
+
+  data = {
+    # TODO
+    username = "TODO"
+    password = random_password.backend-mysql-password.result
+  }
+}
+
+resource "helm_release" "backend-mysql" {
+  name = "backend-mysql"
+  namespace = kubernetes_namespace.backend.metadata[0].name
+  chart = "mysql"
+  repository = local.helm_repository_bitnami
+
+  values = [
+    file("helm_config/backend-mysql.yaml")
+  ]
+
+  set {
+    name = "db.password"
+    value = random_password.backend-mysql-password.result
+  }
+}
