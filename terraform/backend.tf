7a4a1082a297fa038e10a7237c376c8e24023ee2
Milutin Dzunic <milutindzunic@gmail.com>
2020-08-09 12:19:33 +0200

    Continued adding backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -16,9 +16,8 @@ resource "kubernetes_secret" "backend-mysql-access" {
   }
 
   data = {
-    # TODO
-    username = "TODO"
-    password = random_password.backend-mysql-password.result
+    DB_USER = "backend"
+    DB_PASSWORD = random_password.backend-mysql-password.result
   }
 }
 
@@ -36,4 +35,33 @@ resource "helm_release" "backend-mysql" {
     name = "db.password"
     value = random_password.backend-mysql-password.result
   }
+}
+
+resource "kubernetes_config_map" "backend-config" {
+  metadata {
+    name = "backend-config"
+  }
+
+  data = {
+    DB_DRIVER = "mysql"
+    DB_HOST = helm_release.backend-mysql.metadata[0].name
+    DB_PORT = "3360"
+    DB_NAME = "backend"
+  }
+}
+
+resource "helm_release" "backend" {
+  name = "backend"
+  namespace = kubernetes_namespace.backend.metadata[0].name
+  chart = "../charts/backend"
+
+  depends_on = [
+    helm_release.backend-mysql,
+    kubernetes_secret.backend-mysql-access,
+    kubernetes_config_map.backend-config
+  ]
+
+  values = [
+    file("helm_config/backend.yaml")
+  ]
 }
